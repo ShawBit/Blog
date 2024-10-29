57f7989c0f9ffe05cac03e1e40eb404c5e6bcaad
ShawBit <shawbitx@gmail.com>
2024-10-29 16:04:43 +0800

    feat: 更新导航栏组件以使用动态路由，通过引量类型定义增强代码结构；新增路由常量和类型定义文件。

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
-const NavbarItem = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div>{children}</div>;
-};
+import { routes } from "@/constants";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <NavbarItem>Test</NavbarItem>
+    <div className="w-full h-20 px-48 fixed z-50 flex items-center justify-between font-bold">
+      <Link href="/">Shawn求索之旅</Link>
+      <div>
+        <div className="flex gap-8">
+          {routes.length &&
+            routes.map((item) => (
+              <Link href={item.path} key={item.path} className="hover:border-b hover:border-border">
+                {item.title}
+              </Link>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+import type { Route } from "@/types";
+
+export const routes: Array<Route> = [
+  {
+    path: "/about",
+    title: "About",
+  },
+  {
+    path: "/contact",
+    title: "Contact",
+  },
+];
