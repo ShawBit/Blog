23d1a6a7466b27d6a6abc918a27a978a7a6dba75
ShawBit <shawbitx@gmail.com>
2024-10-31 20:24:34 +0800

    feat: Update Home page content and Navbar styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div className="grid items-center justify-items-center min-h-screen relative">Hello World</div>;
+  return (
+    <div className="min-h-screen w-full">
+      <div className="flex flex-col gap-4 px-48 pt-72">
+        <span className="text-3xl font-bold">Welcome to my blog</span>
+        <span className="text-5xl font-bold"> I&apos;m Shawn</span>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 px-48 fixed z-50 flex items-center justify-between font-bold text-xl">
+    <div className="w-full h-24 px-48 fixed z-50 flex items-center justify-between font-bold text-xl">
       <Link href="/">Shawn求索之旅</Link>
       <div>
         <div className="flex gap-8">
           {routes.length &&
             routes.map((item) => (
-              <Link href={item.path} key={item.path} className="hover:border-b hover:border-border">
+              <Link
+                href={item.path}
+                key={item.path}
+                className="hover:border-b hover:border-border"
+              >
                 {item.title}
               </Link>
             ))}
