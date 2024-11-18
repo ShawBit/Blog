98400105328f4dbe60cc4c56c811b4dbb86d1599
ShawBit <shawbitx@gmail.com>
2024-11-18 19:51:05 +0800

    feat: add the blog type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Route {
   path: string;
   title: string;
 }
+
+export type BlogPost = {
+  id?: string;
+  slug?: string;
+  title?: string;
+  content: string;
+  metadata: {
+    [key: string]: any;
+  };
+};
+
+export type PostsByMonth = {
+  [key: string]: BlogPost[];
+};
