ac0ae07aad9690314f1da27262d82c93aee2d839
ShawBit <shawbitx@gmail.com>
2024-11-19 21:46:52 +0800

    feat: remove the desktop from background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { usePathname } from "next/navigation";
-import { Suspense } from "react";
-import DesktopModel from "./DesktopModel";
 import Stars from "./Stars";
 
 const Background = () => {
-  const pathname = usePathname();
   return (
     <div className="fixed top-0 left-0 -z-10 w-screen h-screen">
       <Canvas camera={{ fov: 35, near: 0.1, far: 100, position: [0, 1, 7] }}>
@@ -17,11 +13,6 @@ const Background = () => {
           position={[1, 1, 0]}
         />
         <Stars />
-        {pathname === "/" && (
-          <Suspense fallback={null}>
-            <DesktopModel />
-          </Suspense>
-        )}
       </Canvas>
     </div>
   );
