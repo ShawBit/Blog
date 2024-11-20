75a31fe8d83e7ac97bb5f01f53e397a29fae524c
ShawBit <shawbitx@gmail.com>
2024-11-20 19:09:43 +0800

    chore: add highlight for mdx

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -7,8 +6,4 @@ const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
